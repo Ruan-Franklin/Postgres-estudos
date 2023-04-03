25dd315e4c6b75ec0b4f83f8bdf27921186ae53b
Ruan Franklin <94229859+Ruan-Franklin@users.noreply.github.com>
2023-04-03 10:24:57 -0300

    Adiciona questão 1

---
+++ Gatilhos/exercicios/q1.sql
@@ -0,0 +1,6 @@
+
+/*Q1: Escreva um comando para alterar a tabela authors, incluindo o campo "cpf" do tipo varchar com capacidade para armazenar um número de CPF (11 dígitos).
+*/
+ALTER TABLE authors ADD cpf varchar(11);
+
+
